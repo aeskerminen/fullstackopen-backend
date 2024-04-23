075fd73ad08d480f8d5dc319ad5c96dc35df735a
Artturi <kerminenartturi@gmail.com>
2024-04-23 17:28:21 +0300

    Add frontend code

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(cors())
 
 app.use(express.json());
 
+app.use(express.static('dist'))
+
 morgan.token('content', function (req, res) { return JSON.stringify(req.body['content']) })
 const FORMAT = ":method :url :status :res[content-length] - :response-time ms :content"
 
