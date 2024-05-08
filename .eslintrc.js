f7e7fcab33325eea68a2c103100dcf528b6e258c
Artturi <kerminenartturi@gmail.com>
2024-05-08 14:46:57 +0300

    3.21

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // ...
+    'plugins': [
+        '@stylistic/js'
+    ],
+    'extends': 'eslint:recommended',
+    'rules': {
+        '@stylistic/js/indent': [
+            'error',
+            2
+        ],
+        '@stylistic/js/linebreak-style': [
+            'error',
+            'unix'
+        ],
+        '@stylistic/js/quotes': [
+            'error',
+            'single'
+        ],
+        '@stylistic/js/semi': [
+            'error',
+            'never'
+        ],
+    }
+}
